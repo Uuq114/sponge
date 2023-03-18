fd3ca5334d83ec2d4eaa50a52bf213d55cbbdacc
Uuq114 <811670442@qq.com>
2023-03-18 20:53:40 +0800

    lab2:wrap and unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <iostream>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -10,12 +11,13 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+const uint64_t SEQ_NO_NUM = 1UL << 32;  // 指定1UL是8字节整数
+
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{static_cast<uint32_t>(n) + isn.raw_value()};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,7 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    int32_t offset = n.raw_value() - wrap(checkpoint, isn).raw_value();
+    int64_t ret = checkpoint + offset;
+    return ret >= 0 ? ret : ret + SEQ_NO_NUM;
 }
